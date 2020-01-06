b172c6e2eef4bb10f78c9aa778ba97d53fe775f5
rie1224 <a10ea211@yahoo.co.jp>
2020-01-06 20:20:24 +0900

    setting user_edit
    ユーザー管理機能　問題３まで
    （ユーザー編集ページを作成）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'chats/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "chats#index"
+  devise_for :users
+  resources :users, only: [:edit, :update]
 end
