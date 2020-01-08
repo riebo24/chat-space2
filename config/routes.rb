99243d747d0d009469b6e67cfd5869348a8051f7
rie1224 <a10ea211@yahoo.co.jp>
2020-01-08 17:43:23 +0900

    create model
    groupモデル、group_userモデルを作成。（アソシエーションの設定含む）

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "chats#index"
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, create, edit, update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
